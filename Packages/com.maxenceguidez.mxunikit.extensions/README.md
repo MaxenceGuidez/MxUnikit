2401c4b79c7615210f4b1919736f91ff8fadcf6a
MaxenceGuidez <maxence.guigui@gmail.com>
2026-01-26 13:55:40 +0100

    [EXTENSIONS] Init extensions package

---
+++ Packages/com.maxenceguidez.mxunikit.extensions/README.md
@@ -0,0 +1,27 @@
+﻿# MxUnikit - Extensions
+
+Provides a collection of Unity extension methods for common operations
+
+> WARN: This package is experimental and subject to frequent changes. It is not intended for public use.
+
+## Features
+
+### Transform Extensions
+
+#### DestroyChildren
+Destroys all children of a Transform with optional exclusions.
+
+```csharp
+// Destroy all children
+transform.DestroyChildren();
+
+// Ignore specific transforms
+transform.DestroyChildren(child1, child2, child3);
+
+// Ignore a list of transforms
+List<Transform> keepThese = new List<Transform> { child1, child2 };
+transform.DestroyChildren(keepThese);
+
+// Chain with other operations
+transform.DestroyChildren().SetParent(newParent);
+```
---
+++ Packages/com.maxenceguidez.mxunikit.extensions/Runtime/MxExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MxUnikit.Extensions
+{
+    public static class MxExtensions
+    {
+        #region Destroy Children
+
+        public static Transform DestroyChildren(this Transform transform)
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                Object.Destroy(transform.GetChild(i).gameObject);
+            }
+
+            return transform;
+        }
+
+        public static Transform DestroyChildren(this Transform transform, params Transform[] ignoredTransforms)
+        {
+            return ignoredTransforms is not { Length: > 0 }
+                ? transform.DestroyChildren()
+                : transform.DestroyChildren((IEnumerable<Transform>)ignoredTransforms);
+        }
+
+        public static Transform DestroyChildren(this Transform transform, IEnumerable<Transform> ignoredTransforms)
+        {
+            if (ignoredTransforms is null or ICollection<Transform> { Count: 0 })
+            {
+                return transform.DestroyChildren();
+            }
+
+            HashSet<Transform> ignoredSet = new HashSet<Transform>(ignoredTransforms);
+
+            if (ignoredSet is { Count: 0 })
+            {
+                return transform.DestroyChildren();
+            }
+
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                Transform child = transform.GetChild(i);
+
+                if (!ignoredSet.Contains(child))
+                {
+                    Object.Destroy(child.gameObject);
+                }
+            }
+
+            return transform;
+        }
+
+        #endregion
+    }
+}
